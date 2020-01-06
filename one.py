ea3b042fda63f242edb98ace60164d3ba10507c8
David Eliason <davethemaker@gmail.com>
2020-01-06 15:11:55 -0800

    input text string and numeric, transform to integer, output

--- one.py
+++ one.py
@@ -1,6 +1,14 @@
-print('Hello world!')
+
+# INPUT
+
+# print('Hello world!')
 print('What is your name?') # ask for their name
 myName = input()
-print('It is good to meet you, ' + myName)
-print('The length of your name is:')
-print(len(myName))
+# print('It is good to meet you, ' + myName)
+# print('The length of your name is:')
+# print(len(myName))
+print("how old are you?")
+age = input()
+print(" you will be " + str(int(age) + 1) + "in a year")
+
+
